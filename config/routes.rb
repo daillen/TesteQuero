06a66870cda962d4848d776647a75b8fb5c0ad19
Andre Vaillant <andre.v712@gmail.com>
2018-09-23 20:34:39 -0300

    Add routes for instituicao requests
    GET /instituicao lists all instituicaos
    GET /instituicao/id show an instituicao with given id
    POST /instituicao creates a new instituicao given a JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  # Routes for alunos
   get "/alunos" => "alunos#index"
   get "/alunos/:id" => "alunos#show"
   post "/alunos" => "alunos#create"
+
+  # Routes for instituicaos
+  get "/instituicaos" => "instituicaos#index"
+  get "/instituicaos/:id" => "instituicaos#show"
+  post "/instituicaos" => "instituicaos#create"
+
+  # Routes for matriculas
+
+  # Routes for faturas
 end
