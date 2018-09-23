739642ec078357ac1ed8dab947bcfdddbb878fdf
Andre Vaillant <andre.v712@gmail.com>
2018-09-23 11:41:44 -0300

    Add routes for creation and listing of Alunos
    GET /alunos lists all alunos
    GET /alunos/id show an aluno with given id
    POST /alunos creates a new aluno given a JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/alunos" => "alunos#index"
+  get "/alunos/:id" => "alunos#show"
+  post "/alunos" => "alunos#create"
 end
