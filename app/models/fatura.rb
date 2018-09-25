d7fa85a8d7bf1f76da85a0776ff86a77cc80ba92
Andre Vaillant <andre.v712@gmail.com>
2018-09-25 18:04:55 -0300

    Update fatura model
    Remove the set_defaults method, now using the database default instead

--- app/models/fatura.rb
+++ app/models/fatura.rb
@@ -1,10 +1,4 @@
 class Fatura < ApplicationRecord
-  after_initialize :set_defaults
-
-  def set_defaults
-    @status ||= "Aberta"
-  end
-
   belongs_to :matricula
 
   validates :valor, presence: true, numericality: {greater_than: 0}
