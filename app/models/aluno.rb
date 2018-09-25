be543ca33686c8afb85f93efcb3564c143dda940
Andre Vaillant <andre.v712@gmail.com>
2018-09-24 22:53:41 -0300

    Add validation of presence for email on alunos

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -3,6 +3,7 @@ class Aluno < ApplicationRecord
   has_many :faturas, through: :matriculas
 
   validates :nome, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :cpf, presence: true, uniqueness: true, numericality: true
   validates :genero, presence: true, inclusion: %w(M F)
   validates :pagamento, presence: true, inclusion: %w(Boleto Cartão)
